f6be391e2305b42fc0875cdf49678e387ab9158f
Tinashe <tinashemundieta36@gmail.com>
2025-10-19 19:16:21 +0200

    test: add polyfill for matchMedia in jsdom to support Chakra UI hooks

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom/vitest'
+
+// Polyfill matchMedia for Chakra UI hooks in jsdom
+if (typeof window !== 'undefined' && !window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // deprecated
+    removeListener: () => {}, // deprecated
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  })
+}

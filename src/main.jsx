d3fa0e341ab6b60a22847330d43da166d53f779a
Tinashe <tinashemundieta36@gmail.com>
2026-04-15 00:04:55 +0200

    feat: integrate Vercel Analytics for visitor tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import theme from './theme.js'
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 )

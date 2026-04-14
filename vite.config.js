62f80bc6c15e933a87a6e690d4e7feb80ea4dc26
Tinashe <tinashemundieta36@gmail.com>
2026-04-14 23:55:49 +0200

    fix: remove manualChunks causing circular deps and white screen

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'chakra-vendor': ['@chakra-ui/react', '@emotion/react', '@emotion/styled'],
-          'animation-vendor': ['framer-motion'],
-        },
-      },
-    },
     chunkSizeWarningLimit: 600,
     sourcemap: false,
     minify: 'esbuild',
